64fdaef2214ccc1b188ead1a4e42bf4a41ecc2a1
phantomhand <frank@phantomhand.com>
2013-12-04 22:27:46 -0500

    begin adding song writer functionality: add vars, song array, and event listeners for buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-/* Event listener for pad selection */
-$('.pads').click(function(event){
-	document.getElementById(event.target.id+'-mp3').play();	
+$(document).ready(function() {
+
+	/* Array for looping */
+	var song = Array();
+	
+	/* set Recording status to false by default */ 
+	var recording = false;
+	
+	/* Event listener for record button */	
+	$('#record').click(function(){
+		recording = true;
+	});
+	
+	/* Event listener for pad selection */
+	$('.pad').click(function(){
+		var pressed = (event.target.id+'-note')
+		
+		document.getElementById(pressed).play();
+		
+		/* Recording function */
+		if (recording == true) {
+			song.push(event.target.id+'-note');
+		} 
+		
+		/* Event listener for stop button */	
+		$('#stop').click(function(){
+			recording = false;
+		});
+		
+		/* Event listener for play button */	
+		$('#stop').click(function(){
+			recording = false;
+		});
+				
+	});
+	
 });
-
-
-
-
-	
-	
